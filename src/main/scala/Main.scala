54ed9560a4d39c087c46877309471781d94f426d
Jamie Eisenhart <jamie@jamieei.org>
2013-03-03 08:50:42 -0800

    Speak phrase demo

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,32 @@
-package org.jamieei.hangman
+package org.jamieei.elk
+
+import java.io._
+import java.net._
 
 object Main {
+  //val msg = "Da11001234003F" // arm away
+  val msg = "09sp12300BE" // phrase 123
+
   def main(args: Array[String]) {
-    println("hello ELK")
+    val ip = sys.env("ELK")
+    val port = 2101
+    val code = sys.env("ELK_CODE")
+    println(s"Connecting to ELK at $ip:$port using code $code")
+
+    val socket = new Socket(ip, port)
+    val out = new PrintWriter(socket.getOutputStream, true)
+    //val isr = new InputStreamReader(socket.getInputStream)
+    //val in = new BufferedReader(isr)
+
+    try {
+      out.print(msg + "\r\n")
+      // send command
+      // wait for response
+      // print response
+    } finally {
+      out.close
+      socket.close
+    }
   }
 }
+
