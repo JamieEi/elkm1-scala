dda80191ea18a63a9fd7d4bcf028bfc7b51e84ad
Jamie Eisenhart <jamie@jamieei.org>
2013-03-03 12:03:54 -0800

    Basic read message

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,27 +4,35 @@ import java.io._
 import java.net._
 
 object Main {
-  //val msg = "Da11001234003F" // arm away
-  val msg = "09sp12300BE" // phrase 123
-
   def main(args: Array[String]) {
     val ip = sys.env("ELK")
     val port = 2101
     val code = sys.env("ELK_CODE")
     println(s"Connecting to ELK at $ip:$port using code $code")
 
-    val socket = new Socket(ip, port)
-    val out = new PrintWriter(socket.getOutputStream, true)
-    //val isr = new InputStreamReader(socket.getInputStream)
-    //val in = new BufferedReader(isr)
+    //val msg = s"Da1100${code}003F" // arm away
+    //val msg = "09sp12300BE" // phrase 123
+    val msg = "09ld00100D6" // request system log data
 
+    val socket = new Socket(ip, port)
     try {
+      val out = new PrintWriter(socket.getOutputStream, true)
+      val isr = new InputStreamReader(socket.getInputStream)
+      val in = new BufferedReader(isr)
+
+      println("SEND: " + msg)
       out.print(msg + "\r\n")
-      // send command
-      // wait for response
-      // print response
-    } finally {
+
+      Thread.sleep(1000)
+
+      val response = in.readLine
+      println("RESPONSE: " + response)
+
+      socket.shutdownOutput
       out.close
+      in.close
+      println("Done!")
+    } finally {
       socket.close
     }
   }
